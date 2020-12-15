e38f584c22d4bae94428f0df3cddc0db81ed98cc
Engin Can Höke <hoke@engincan.local>
2020-12-15 22:25:09 +0300

    bug fix

--- utility/xlogin.php
+++ utility/xlogin.php
@@ -1,7 +1,4 @@
 <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
   $username= $_POST['name'];
   $password= $_POST['pass'];
   $conn= new mysqli('localhost','phpmyadmin','phpmyadmiN@123','users');
--- utility/xsignup.php
+++ utility/xsignup.php
@@ -1,33 +1,4 @@
 <?php
-  // ini_set('display_errors', 1);
-  // ini_set('display_startup_errors', 1);
-  // error_reporting(E_ALL);
-  // $username= $_POST['name'];
-  // $password= $_POST['pass'];
-  // $email= $_POST['email'];
-
-  // $conn= new mysqli('localhost','phpmyadmin','phpmyadmiN@123','users');
-  // if(!$conn){
-  //     die("CONNECTION FAILED".mysqli_connect_error());
-  // }
-  // $sql="INSERT INTO users.users (name, pass, email) VALUES ('$username', '$password', '$email')";
-  // $result=mysqli_query($conn,$sql);
-  // echo $result;
-
-  // if($result === 1) {
-  //   echo("Sign up succeeded");
-  // }
-  // else {
-  //   echo("Sign up  Failed");
-  // }
-  // echo("------");
-  // echo mysqli_error($conn);
-  // echo("------");
-  // mysqli_close($conn)
-// <?php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
   $username= $_POST['name'];
   $password= $_POST['pass'];
   $email= $_POST['email'];
