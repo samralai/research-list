cb9f70db194e6cc37dcc21716fa01856b9327420
slmsertbas <56273183+slmsertbas@users.noreply.github.com>
2020-12-23 23:33:09 +0300

    Add files via upload

--- utility/content1.php
+++ utility/content1.php
@@ -43,9 +43,12 @@ mysqli_close($con);
                     <td>
                         <table width="380" border="0" cellpadding="10" cellspacing="10" align="right">
                             <h3>Title:  <?php 
+                            $con=mysqli_connect("localhost","phpmyadmin","phpmyadmiN@123","articles");
                             while($row = mysqli_fetch_array($selected_title)) {
                                 echo "<td>" . $row['title'] . "</td>";
+                                
                             }  
+                            mysqli_close($con);
                             ?>  </h3><br/> <!-- select title from articles where id=1; -->
                             <h5>Author: <?php echo $selected_author ?></h5><br/> <!-- select author from articles where id=1; -->
                             <h5>DOI: <?php echo $selected_doi ?></h5><br/> <!-- select doi from articles where id=1; -->
