267ef4add662bb90dd22bad87d7214e0d48c02e3
Engin Can Höke <engincan_hoke@gmail.com>
2021-01-09 22:00:43 +0300

    bug fix

--- utility/xupdate.php
+++ utility/xupdate.php
@@ -5,22 +5,31 @@
   $email= $_POST['email'];
   $fullname= $_POST['fullname'];
   $id= $_SESSION['id'];
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo("Invalid email format");
-  } else {
-    $conn= new mysqli('localhost','phpmyadmin','phpmyadmiN@123','users');
-    if(!$conn){
-        die("CONNECTION FAILED".mysqli_connect_error());
-    }
-    $sql="UPDATE users.users SET name = '$username', pass = '$password', email = '$email', fullname = '$fullname' WHERE (id = '" . $id . "')";
-    $result=mysqli_query($conn,$sql);
-    if($result == 1) {
-      echo $sql;
-      echo "Update succeeded";
-    }
-    else {
-      echo "Update failed";
-    }
-    mysqli_close($conn);
+
+  $conn= new mysqli('localhost','phpmyadmin','phpmyadmiN@123','users');
+  if(!$conn){
+      die("CONNECTION FAILED".mysqli_connect_error());
   }
+
+  $checkvalue = "SELECT * FROM users.users WHERE id != ('" . $id . "') AND (name='$username' OR email='$email')";
+  $checkresult=mysqli_query($conn,$checkvalue);
+
+  if($checkresult->num_rows > 0) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      echo("Invalid email format");
+    } else {
+      $sql="UPDATE users.users SET name = '$username', pass = '$password', email = '$email', fullname = '$fullname' WHERE (id = '" . $id . "')";
+      $result=mysqli_query($conn,$sql);
+      if($result == 1) {
+        echo "Update succeeded";
+      }
+      else {
+        echo "Update failed";
+      }
+    }
+  }
+  else {
+    echo "Error - Duplicated Username or Email";
+  }
+  mysqli_close($conn);
 ?>
