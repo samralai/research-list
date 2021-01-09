3713a99640650e3248c5d2c3bd8b8e5adcc90d9c
Engin Can Höke <40316525+engincanhoke@users.noreply.github.com>
2021-01-09 17:32:24 +0300

    Create xupdate.php

---
+++ utility/xupdate.php
@@ -0,0 +1,23 @@
+<?php
+  $username= $_POST['name'];
+  $password= $_POST['pass'];
+  $email= $_POST['email'];
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("Invalid email format");
+  } else {
+    $conn= new mysqli('localhost','phpmyadmin','phpmyadmiN@123','users');
+    if(!$conn){
+        die("CONNECTION FAILED".mysqli_connect_error());
+    }
+    $sql="UPDATE users.users SET name = '$username', pass = '$password', email = '$email' WHERE (id = '1')";
+    $result=mysqli_query($conn,$sql);
+    if($result == 1) {
+      echo "Update succeeded";
+    }
+    else {
+      echo "Update failed";
+    }
+    mysqli_close($conn);
+  }
+?>
